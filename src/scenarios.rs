c79143be7c59cef8ea50ec3948d96b57a92d739a
1Samuel17 <samuel616007@gmail.com>
2026-05-27 20:03:01 -0500

    added get_diff fn with test for scenario enum

--- src/scenarios.rs
+++ src/scenarios.rs
@@ -24,6 +24,19 @@ impl Scenario {
             Self::Hypothetical { .. } => "Hypothetical".to_string(),
         }
     }
+
+    pub fn get_diff(&self) -> i32 {
+        match self {
+            Self::Real { incomes, obligations, note } | 
+            Self::Hypothetical { incomes, obligations, note } => {
+                let total_income = incomes.iter().map(|i| i.get_amount()).fold(0, |acc, a| acc + a );
+                let total_obligations = obligations.iter().map(|i| i.get_amount()).fold(0, |acc, a| acc + a );
+
+                total_income - total_obligations
+            }
+        }
+    }
+
 }
 
 impl Display for Scenario {
@@ -70,8 +83,9 @@ impl Scenarios {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::incomes::Incomes;
-    use crate::obligations::Obligations;
+    use crate::incomes::{Income, Incomes};
+    use crate::obligations::{self, Obligations};
+    use crate::utilities::Frequency::{self, Random};
 
     #[test]
     fn test_new_scenario() {
@@ -95,5 +109,22 @@ mod tests {
             }
             _ => panic!("test failed"),
         }
+    }
+
+    #[test]
+    fn test_get_diff() {
+        let mut incomes = Incomes::new();
+        incomes.push(Income::Person { description: (String::from("Someone")), amount: (50), frequency: (Random), note: (String::from("Zelle")) });
+        incomes.push(Income::Person { description: (String::from("Someone")), amount: (50), frequency: (Random), note: (String::from("Zelle")) });
+        incomes.push(Income::Person { description: (String::from("Someone")), amount: (50), frequency: (Random), note: (String::from("Zelle")) });
+        let mut obligations = Obligations::new();
+        obligations.push(Obligation::CreditCard { amount: (51), frequency: (Random), note: (String::from("a dollar short")) });
+        obligations.push(Obligation::CreditCard { amount: (51), frequency: (Random), note: (String::from("a dollar short")) });
+        obligations.push(Obligation::CreditCard { amount: (51), frequency: (Random), note: (String::from("a dollar short")) });
+        let scenario = Scenario::Hypothetical { incomes: (incomes), obligations: (obligations), note: (String::from("diff should be negative 3")) };
+
+        println!("{}", scenario.get_diff());
+
+        assert_eq!(scenario.get_diff(), -3);
     }
 }
